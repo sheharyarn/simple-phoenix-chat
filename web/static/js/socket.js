a8a2aae9820e338d74810fc3de54d1d68c8e9de1
Sheharyar Naseer <hello@sheharyar.me>
2016-05-23 00:25:05 +0500

    Handling channel connections on the client-side

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -54,7 +54,25 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel("lobby", {});
+let list    = $('#message-list');
+let message = $('#message');
+let name    = $('#name');
+
+message.on('keypress', event => {
+  if (event.keyCode == 13) {
+    console.log(event)
+    channel.push('new_message', { name: name.val(), message: message.val() });
+    message.val('');
+  }
+});
+
+channel.on('new_message', payload => {
+  list.append(`<b>${payload.name || 'Anonymous'}:</b> ${payload.message}<br>`);
+  list.prop({scrollTop: list.prop("scrollHeight")});
+});
+
+
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
